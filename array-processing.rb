f18b8555ddf60cb6ee2580b6298149f57b570c4c
Alt70155 <chika1086@gmail.com>
2018-08-24 00:35:56 +0900

    add 配列問題

---
+++ array-processing.rb
@@ -0,0 +1,24 @@
+# 整数が格納された配列numとその長さlengthを引数として受け取り、結果を返す関数sum_num
+#
+# ①処理対象の先頭要素から順に、要素とその隣の要素の値を加算する処理を、最後まで繰り返す。
+# ②処理対象の要素を一つ減らす
+# ③処理対象の要素が一つになるまで繰り返し、加算した結果を返却する
+
+
+num = [40, 30, 14, 94]
+length = num.length
+
+def sum_num(num, length)
+  ct_len = length - 1
+  loop do
+    break if ct_len == 0
+    for i in 0...ct_len
+      num[i] = num[i] + num[i + 1]
+    end
+    num.delete_at(ct_len)
+    ct_len -= 1
+  end
+  return num[0]
+end
+
+p sum_num(num, length)
