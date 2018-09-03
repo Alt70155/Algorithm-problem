072da8101c657b35c4fe359ee0abf27f16ea53af
chika <chika1086@gmail.com>
2018-09-03 22:37:25 +0900

    add wordsearch

--- Search/LinearSearch.rb
+++ Search/LinearSearch.rb
@@ -9,7 +9,7 @@ class Index1
   def self.add_word(index1, words)
     index1[0] = Index1.new('', -1)
     words.each_with_index { |n, i|
-      index1 << Index1.new(n, i)
+      index1 << Index1.new(n, i + 1)
     }
   end
 end
@@ -26,7 +26,7 @@ class Index2
   def self.add_page(index2, words, pages)
     index2[0] = Index2.new('', -1, -1)
     pages.each_with_index { |n, i|
-      index2 << Index2.new(words[i], n, i)
+      index2 << Index2.new(words[i], n, i + 1)
     }
   end
 end
@@ -35,28 +35,39 @@ end
 index1 = []
 index2 = []
 
-words = ['accident', 'agent', 'yurukyan', 'alt70155']
-pages = [300, 301, 302, 303]
+words = ['accommodation', 'acceptable', 'accident', 'agent', 'app', 'a']
+pages = [300, 301, 302, 303, 304, 301]
 
 Index1.add_word(index1, words)
 Index2.add_page(index2, words, pages)
 
 word = 'yurukyan'
-ret = [302, 2]
+ret = Array.new(2)
 
-def word_search(index1, word, ret)
-  flg = 0
-  (1...index1.length).each { |idx1|
-    break unless flg == 0
-    if word == index1[idx1].key1
-      flg = 1
-    else
-      if word.length < index1[idx1].key1.length
-        flg = 2
-      end
-    end
-  }
-  flg
-end
+# def word_search(index1, word, ret)
+#   flg, idx1, idx2 = 0, 0, 0
+#   (1...index1.length).each { |idx1_|
+#     break unless flg == 0
+#     if word == index1[idx1_].key1
+#       flg = 1
+#       idx1 = idx1_
+#     else
+#       if word.length < index1[idx1_].key1.length
+#         flg = 2
+#         idx1 = idx1_
+#       end
+#     end
+#   }
+#
+#   if flg > 0
+#     if flg == 1
+#       idx2 = index1[idx1].pt
+#     else
+#       flg = 0
+#       idx2 =
+# end
+#
+# p word_search(index1, word, ret)
 
-p word_search(index1, word, ret)
+
+p index1[3].pt - 1
--- test.rb
+++ test.rb
@@ -1,13 +1,4 @@
-#配列aの値を順に比較し、降順に順位付けする。
-#その結果を配列bに格納し返却する関数ranking1
-#値が同じ場合は添え字の小さいほうを小さい順位にする
-
-arr_a = [65, 37, 48, 92, 37, 54, 20, 65, 37, 81]
-arr_b = Array.new(arr_a.length, 1)
-
-def ranking1(a, b, len)
-  (len).times { |i| ((i.succ)...len).each { |j| a[i] < a[j] ? b[i] += 1 : b[j] += 1 } }
-  arr_b
-end
-
-p ranking1(arr_a, arr_b, arr_a.length)
+a = {'yen' => 400}
+a.each_with_index { |(key, num), i|
+  p key, num, i + 1
+}
